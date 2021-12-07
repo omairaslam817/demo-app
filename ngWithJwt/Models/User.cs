ab531a0f31f13443816244e383d0212ed3b59c58
Omair Aslam <omair.aslam@itsolution24x7.net>
2021-12-07 09:52:00 +0500

    Add project files.

---
+++ ngWithJwt/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ngWithJwt.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
